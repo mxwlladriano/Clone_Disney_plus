a473f2a3dfd62db51ebdde21d4d43d5d2e53bc75
mxwlladriano <mxwlladriano@gmail.com>
2023-08-07 22:42:12 -0300

    setup do projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-function styles() {
+
+
+function styles () {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
-
 exports.default = styles;
-exports.watch = function(){
+exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
