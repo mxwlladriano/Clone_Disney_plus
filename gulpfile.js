162fad6a1855d9a6b579271e8514456533506e16
mxwlladriano <mxwlladriano@gmail.com>
2023-09-18 17:39:08 -0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,20 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 
-
-
-function styles () {
+function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
 
-function images () {
+function images() {
     return gulp.src('./src/images/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'));
 }
 
-
 exports.default = gulp.parallel(styles, images);
 
-
-exports.watch = function() {
+exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
--- src/main.js
+++ src/main.js
@@ -1,33 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const buttons = document.querySelectorAll('[data-tab-button]');
-    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function(botao){
-            const abaAlvo = botao.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
-            escondeTodasAbas();
-            aba.classList.add('shows__list--is--active');
-            removeBotaoAtivo();
-            botao.target.classList.add('shows__tabs__button--is--active');
-        })
+
+
+    for (let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta);
     }
 })
 
-function removeBotaoAtivo(){
-    const buttons = document.querySelectorAll('[data-tab-button]');
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+   const elementoPai = elemento.target.parentNode;
 
-    for (let i = 0; i <buttons.length; i++){
-        buttons[i].classList.remove('shows__tabs__button--is--active');
-    }
-}
-
-
-
-function escondeTodasAbas(){
-    const tabsContainer = document.querySelectorAll('[data-tab-id]');
-
-    for (let i = 0; i < tabsContainer.length; i++) {
-        tabsContainer[i].classList.remove('shows__list--is--active')
-    }
+   elementoPai.classList.toggle(classe);
 }
